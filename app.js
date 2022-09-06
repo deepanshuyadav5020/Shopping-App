17022de1b50fb1cc00a722253b4a872467b1562d
deepanshuyadav5020 <111081491+deepanshuyadav5020@users.noreply.github.com>
2022-09-06 20:40:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const URL ='mongodb+srv://deepanshu_y4246:Anything2_@supplimentsshop.ohclpjg.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(URL,{maxPoolSize:5},(err)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log('Db connection established')
-    }
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const URL =
+  "mongodb+srv://deepanshu_y4246:Anything2_@supplimentsshop.ohclpjg.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(URL, { maxPoolSize: 5 }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Db connection established");
+  }
 });
-module.exports= mongoose;
+module.exports = mongoose;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/product/' , require("./routes/product.route"));
+app.use("/api/product/", require("./routes/product.route"));
 
-app.listen(5000,()=>{
-    console.log("connection done on 5000")
+app.listen(5000, () => {
+  console.log("connection done on 5000");
 });
